b917e131a10aaade75679beb9a6c67aaf4a5fe5d
AshikurRahmanMunna <ashikurrahmanmunna3@gmail.com>
2022-06-25 16:34:11 +0600

    Instance methods

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -5,8 +5,8 @@ const mongoose = require("mongoose");
 const Todo = new mongoose.model("Todo", todoSchema);
 
 // get all todos
-router.get("/", async (req, res) => {
-  await Todo.find({})
+router.get("/", (req, res) => {
+  Todo.find({})
     .select({
       _id: 0,
     })
@@ -20,32 +20,54 @@ router.get("/", async (req, res) => {
     });
 });
 
-// get a todo by id
-router.get("/:id", async (req, res) => {
-  await Todo.find({ _id: req.params.id }).exec((err, data) => {
-    if (err) {
-      res.status(500).json({ error: "Server Side error" });
-    } else {
-      res.status(200).json({ success: true, data });
+// get active todos
+router.get("/active", async (req, res) => {
+  const todo = new Todo();
+  const data = await todo.findActive();
+  res.status(200).json({
+    data
+  })
+});
+
+// get active todos with callback
+router.get("/active-callback", (req, res) => {
+  const todo = new Todo();
+  todo.findActiveCallback((err, data) => {
+    if(err) {
+      res.status(500).json({error: 'Server Side Error'})
+    }
+    else {
+      res.status(200).json({success: true, data})
     }
   });
-  // try {
-  //     const data = await Todo.find({_id: req.params.id})
-  //     res.status(200).json({success: true, data})
-  // }
-  // catch(err) {
-  //     res.status(500).json({success: false, message: 'Internal Server error'})
-  // }
+});
+
+// get a todo by id
+router.get("/:id", async (req, res) => {
+  // Todo.find({ _id: req.params.id }).exec((err, data) => {
+  //   if (err) {
+  //     res.status(500).json({ error: "Server Side error" });
+  //   } else {
+  //     res.status(200).json({ success: true, data });
+  //   }
+  // });
+
+  try {
+    const data = await Todo.find({ _id: req.params.id });
+    res.status(200).json({ success: true, data });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Internal Server error" });
+  }
 });
 
 // post a todo
 router.post("/", async (req, res) => {
   const newTodo = new Todo(req.body);
-  await newTodo.save((err) => {
+  await newTodo.save((err, data) => {
     if (err) {
       res.status(500).json({ error: "There was a error in the server" });
     } else {
-      res.status(200).json({ message: "ToDo was inserted successfully" });
+      res.status(200).json({ message: "ToDo was inserted successfully", data });
     }
   });
 });
